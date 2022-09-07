75b396fe70115527499da9f2e23ac0822889496b
fndsz <45010248+fndsz@users.noreply.github.com>
2022-09-07 14:19:41 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,15 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
 ###### Modify default IP
 sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
 
 ###### Modify hostname
-sed -i 's/OpenWrt/Mi-R3G/g' package/base-files/files/bin/config_generate
+sed -i 's/openwrt/XiaomiR3G/g' package/base-files/files/bin/config_generate
 
 ####### Modify the version number
 sed -i "s/OpenWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 ####### Set argon as default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
