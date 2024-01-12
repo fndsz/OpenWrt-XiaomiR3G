03f869df81df2418d123e4abe3b0041051b94200
fndsz <45010248+fndsz@users.noreply.github.com>
2024-01-13 00:32:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,18 +10,21 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-###### 修改IP
+# 修改默认IP
 sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
 
-###### 修改设备名称
+# 修改主机名称
 sed -i 's/OpenWrt/Mi-R3G/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为MIR3G
 #sed -i 's/ssid=OpenWrt/ssid=MIR3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#开启MU-MIMO
+# 开启MU-MIMO
 #sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-###### Set argon as default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 取消bootstrap为默认主题
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
