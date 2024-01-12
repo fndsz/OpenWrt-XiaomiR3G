3b8c0f5e3dde965351310b4f5a5cf576a28086f8
fndsz <45010248+fndsz@users.noreply.github.com>
2024-01-12 20:40:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,5 +27,3 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 pushd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
-popd
-#=================================================
