9c9f4f341ad94eb9f6df941ab9425392a31a097d
fndsz <45010248+fndsz@users.noreply.github.com>
2024-01-12 10:35:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,14 @@ sed -i 's/OpenWrt/Mi-R3G/g' package/base-files/files/bin/config_generate
 
 ###### Set argon as default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+#=================================================
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+#=================================================
