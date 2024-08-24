2e26d3549600a1ad27e54e1130a357a1f0c4f720
fndsz <45010248+fndsz@users.noreply.github.com>
2024-08-24 09:41:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
- sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
+ #sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
  #sed -i '$a src-git YSFClients https://github.com/qzyuebing/YSFClients.git' feeds.conf.default
  #sed -i '$a src-git YSF2DMR https://github.com/ea5gvk/YSF2DMR.git' feeds.conf.default
  sed -i '$a src-git mmdvm-feeds https://github.com/hamclub/mmdvm-feeds.git' feeds.conf.default
