aaa238a9a24ce8b3b74840617e5789b7316e3046
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-07 18:18:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,6 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
-
 # Add a feed source
 git clone https://github.com/fndsz/mmdvm-op package/mmdvm/mmdvm-openwrt
 git clone https://github.com/siwind/luci-app-wolplus package/wolplus
