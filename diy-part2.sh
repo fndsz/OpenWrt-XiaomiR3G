85be97555d5e54f04d7da00ec91db0fdf5af03c2
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-12 20:08:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# ----------files/ 大法覆盖（优先级最高，推荐） ----------
+mkdir -p files/etc/init.d
+if [ -f "package/feeds/packages/ttyd/files/etc/init.d/ttyd" ]; then
+    cp package/feeds/packages/ttyd/files/etc/init.d/ttyd files/etc/init.d/ttyd
+    chmod 755 files/etc/init.d/ttyd
+    echo "✅ copied ttyd init.d -> files/etc/init.d/ttyd (755)"
+fi
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
