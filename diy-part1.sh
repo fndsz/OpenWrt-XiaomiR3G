3e3a2f56dd1d16f10d976d2b93ed86bce0af119d
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-15 17:43:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-git clone https://github.com/fndsz/mmdvm-openwrt.git package/mmdvm
-git clone https://github.com/fndsz/mmdvmpatch.git package/mmdvm
+git clone https://github.com/fndsz/mmdvm-openwrt package/mmdvm
+#git clone https://github.com/fndsz/mmdvmpatch package/mmdvm
 
 # Add a feed source
 #git clone https://github.com/fndsz/mmdvm-op package/mmdvm
