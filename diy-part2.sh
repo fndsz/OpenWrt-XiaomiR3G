547e04e7849289906ed6020baf9f451c45af7a13
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-17 19:28:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,3 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 
 # 修改主机名称
 sed -i 's/ImmortalWrt/Mi-R3G/g' package/base-files/files/bin/config_generate
-
-# 修复 dnsmasq 冲突
-sed -i 's/CONFIG_PACKAGE_dnsmasq=y/# CONFIG_PACKAGE_dnsmasq is not set/' .config
-sed -i 's/# CONFIG_PACKAGE_dnsmasq-full is not set/CONFIG_PACKAGE_dnsmasq-full=y/' .config
