c43caa48b061872de9629b28938d030c25c2d5f4
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-17 19:29:22 +0800

    Update feed source and add mmdvm-op package

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,8 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+git clone https://github.com/fndsz/mmdvm-op package/mmdvm/mmdvm-openwrt
 git clone https://github.com/siwind/luci-app-wolplus package/wolplus
